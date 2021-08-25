a261fcf2b95ab385349f7fcea1dc7b7fcb2e98a8
Winges4k <87330083+Winges4k@users.noreply.github.com>
2021-08-26 01:48:59 +0200

    Update ch1.py

--- sh/scripts/ch1.py
+++ sh/scripts/ch1.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u8')
+            print('https://raw.githubusercontent.com/Winges4k/youtube-iptv/main/assets/moose_na.m3u8')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u8')
+            print('https://raw.githubusercontent.com/Winges4k/youtube-iptv/main/assets/moose_na.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
